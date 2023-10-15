acb5283951ca2bf4e4da48d5d98009ba6fdd4220
Dona Siby <dona.c.siby@gmail.com>
2023-10-15 18:56:05 +0530

    some more questions done

---
+++ JavaDS/src/b.java
@@ -0,0 +1,12 @@
+public class b {
+    public static void main(String[] args) {
+        String string1 = "abcdefg";
+        String string2 = "hin";
+        if (string1.indexOf(string2)!=-1){
+            System.out.println("Substring");
+        }
+        else {
+            System.out.println("Not a substring");
+        }
+    }
+}
---
+++ JavaDS/src/ques10.java
@@ -0,0 +1,33 @@
+//Given an array of integers nums which is sorted in
+//ascending order, and an integer target, write a
+//function to search target in nums. If target exists,
+//then return its index. Otherwise, return -1. You must
+//write an algorithm with O(log n) runtime
+//complexity.
+
+import java.util.Arrays;
+
+public class ques10 {
+    public static void main(String[] args) {
+        int[] array ={1,3,5,2,4,7};
+        Arrays.sort(array);
+        System.out.println("Sorted array is "+Arrays.toString(array));
+        int target=9;
+        int result=findTarget(target,array);
+        if (result==-1){
+            System.out.println("Target not present in the array");
+        }
+        else{
+            System.out.println("Target "+target+" present at index "+result);
+        }
+    }
+    public static int findTarget(int num,int[] array){
+        int size = array.length;
+        for (int i=0;i<size;i++){
+            if (array[i]==num){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
