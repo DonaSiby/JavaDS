cd723c5a35d88964134d9424a620c7cdf4bb49a1
Dona Siby <dona.c.siby@gmail.com>
2023-10-29 20:15:37 +0530

    optimising code

--- JavaDS/src/oop/boxDemo.java
+++ JavaDS/src/oop/boxDemo.java
@@ -4,14 +4,13 @@ class Box {
   double depth;
   
   double volume(double height, double width, double depth) {
-    double volume = height * width * depth;
-    return volume;
+    return width*height*depth;
   }
 }
 
 public class boxDemo {
   public static void main(String[] args) {
     Box box1 = new Box();
-    System.out.println(box1.volume(10, 20, 30));
+    System.out.println("Vol is :"+box1.volume(10, 20, 30));
   }
 }
